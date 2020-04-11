6d4fa616d93a3b81e6fab5ced4e75f2863fc83e7
David Szuhai <szuhid@gmail.com>
2020-04-11 22:54:07 +0200

    Progress

--- src/main/java/user/Main.java
+++ src/main/java/user/Main.java
@@ -1,4 +1,22 @@
 package user;
 
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+
+import java.time.LocalDate;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Jdbi jdbi = Jdbi.create("jdbc:h2:mem:test");
+        jdbi.installPlugin(new SqlObjectPlugin());
+        try (Handle handle = jdbi.open()) {
+            UserDao dao = handle.attach(UserDao.class);
+            dao.createTable();
+            dao.insert(new User("alma", "kutyus", "Kis Béla", "alma@gmail.com", User.Gender.MALE, LocalDate.of(2015,02,05), true));
+            dao.insert(new User("zold", "kek", "Ló Fej", "csiko@gmail.com", User.Gender.FEMALE, LocalDate.of(2003,10,15), true));
+            dao.listUsers().stream().forEach(System.out::println);
+        }
+    }
 }
--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,4 +1,85 @@
 package user;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+
+//@AllArgsConstructor
+//@NoArgsConstructor
+@Builder
+@Data
 public class User {
+
+    public static enum Gender {
+        FEMALE,
+        MALE
+    }
+
+    private Long id;
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+    private Gender gender;
+    private LocalDate dob;
+    private boolean enabled;
+
+    public User(){}
+
+    public User(Long id, String username, String password, String name, String email, Gender gender, LocalDate dob, boolean enable){
+        this.id = id;
+        this.username =username;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+        this.dob = dob;
+        this.enabled = enable;
+    }
+
+    public User(String username, String password, String name, String email, Gender gender, LocalDate dob, boolean enabled){
+        this(null, username, password, name, email, gender, dob, enabled);
+    }
+    public Long getId(){return id;}
+    public void setId(Long id){this.id = id;}
+
+    public String getUsername(){return username;}
+    public void setUsername(String username){this.username = username;}
+
+    public String getPassword(){return password;}
+    public void  setPassword(String password){this.password = password;}
+
+    public String getName(){return name;}
+    public void setName(String name){this.name = name;}
+
+    public String getEmail(){return email;}
+    public void setEmail(String email){this.email = email;}
+
+    public Gender getGender(){return gender;}
+    public void setGender(Gender gender){this.gender = gender;}
+
+    public LocalDate getDob(){return dob;}
+    public void setDob(LocalDate dob){this.dob = dob;}
+
+    public boolean getEnable(){return enabled;}
+    public void setEnabled(boolean enabled){this.enabled = enabled;}
+
+    @Override
+    public String toString(){
+        return "User{" +
+                "id:" +id+
+                "username:" +username+
+                "password:" +password+
+                "name:" +name+
+                "email:" +email+
+                "gender:" +gender+
+                "date of birth:" +dob+
+                "enabled:" +enabled+
+                '}';
+    }
+
 }
